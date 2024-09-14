4c85091f7b449d1f1c492f2b37247b99b69bc25d
Tom <tomlazore@gmail.com>
2024-09-13 20:29:56 -0400

    socials done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
