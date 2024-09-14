0cafc3c404aa3fd561e210dab9ead62833fcef54
Tom <tomlazore@gmail.com>
2024-09-13 22:02:45 -0400

    merged components into home componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { SocialComponent } from './social/social.component';
-import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SocialComponent,
-    FooterComponent,
     MenuComponent
   ],
   imports: [
