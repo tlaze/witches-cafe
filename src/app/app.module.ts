93382e02d6ed68052e459db105302c8888d84dd1
Tom <tomlazore@gmail.com>
2024-09-19 18:37:53 -0400

    separated nav and footer into own componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
